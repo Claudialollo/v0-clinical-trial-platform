f5326b62747c014494682db3d34009a18c0ea0ee
Claudialollo <claudialollobattista@gmail.com>
2026-04-14 20:12:47 +0200

    Update nebix-splash.tsx

--- components/splash/nebix-splash.tsx
+++ components/splash/nebix-splash.tsx
@@ -53,8 +53,8 @@ export function NebixSplash({ onEnter }: NebixSplashProps) {
               transition={{ type: "spring", duration: 1.2, bounce: 0.4 }}
               className="relative"
             >
-              <div className="relative flex items-center justify-center w-96 h-96">
-                <img src="/nebix-logo.png" alt="NEBix Logo" className="w-96 h-96 object-contain" />
+              <div className="relative flex items-center justify-center w-150 h-150">
+                <img src="/nebix-logo.png" alt="NEBix Logo" className="w-150 h-150 object-contain" />
               </div>
             </motion.div>
 
